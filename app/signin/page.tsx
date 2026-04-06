a018e0a1b2efbf5c7c3125e57e67b674388cdcb2
matus19111-gif <matus19111@gmail.com>
2026-04-06 23:08:52 +0600

    Fix indentation and formatting in signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 "use client";
 
+import React from "react";
 import Link from "next/link";
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
@@ -221,4 +221,4 @@ export default function Login() {
       </div>
     </main>
   );
-}
+        }
