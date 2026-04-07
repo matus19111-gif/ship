b93264c65e59ae9f49989956481c31d9c8b930f8
matus19111-gif <matus19111@gmail.com>
2026-04-07 13:27:08 +0600

    Delete app/dashboard/page.tsx

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/dashboard/page.js
-import ButtonAccount from "@/components/ButtonAccount";
-import DashboardClient from "./_components/DashboardClient";
-
-export const dynamic = "force-dynamic";
-
-export default async function Dashboard() {
-  return <DashboardClient />;
-}
