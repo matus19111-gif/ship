d4120d577bb3bac32e845db2984454c11b657448
matus19111-gif <matus19111@gmail.com>
2026-04-08 22:37:32 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react'
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
 import FeaturesAccordion from "@/components/FeaturesAccordion";
 import Pricing from "@/components/Pricing";
@@ -14,7 +15,7 @@ export default function Home() {
         <Header />
       </Suspense>
       <main>
-        
+        <Hero />
         <Problem />
         <FeaturesAccordion />
         <Pricing />
