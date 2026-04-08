e2574c8b47778e46e9bbe118d862024ca91f82db
matus19111-gif <matus19111@gmail.com>
2026-04-08 22:32:21 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <Header />
       </Suspense>
       <main>
-        <Hero />
+        
         <Problem />
         <FeaturesAccordion />
         <Pricing />
