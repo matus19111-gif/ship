effe121251f7688d45fcde844006fe723dd52a84
matus19111-gif <matus19111@gmail.com>
2026-04-14 13:24:42 +0600

    Create index.ts

---
+++ components/social-proof/index.ts
@@ -0,0 +1,5 @@
+'use client'
+
+export { SocialProofProvider, useSocialProof } from './SocialProofContext'
+export { default as SocialProofPopup } from './SocialProofPopup'
+export { NotificationContent } from './NotificationCards'
