867c5e58a8ddfc33c126ebd26d0f432def589acc
matus19111-gif <matus19111@gmail.com>
2026-04-16 13:18:03 +0600

    Delete components/social-proof directory

--- components/social-proof/index.ts
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export { SocialProofProvider, useSocialProof } from './SocialProofContext'
-export { default as SocialProofPopup } from './SocialProofPopup'
-export { NotificationContent } from './NotificationCards'
